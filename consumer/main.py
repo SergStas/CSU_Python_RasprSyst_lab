c46cf22bbbe4a447a8e2298244e5a6e8559e5efe
stasishensergey <sergstas0@gmail.com>
2022-11-24 02:07:49 +0500

    Lab 5

--- consumer/main.py
+++ consumer/main.py
@@ -2,7 +2,9 @@ import sys
 import pika
 import requests
 import json
+from typing import Optional
 from os import getenv
+import redis
 
 
 USER = getenv("RABBITMQ_DEFAULT_USER")
@@ -15,14 +17,39 @@ WEB_PORT = '80'
 CONNECTION_URL = f'amqp://{USER}:{PASSWORD}@{BROKER_HOSTNAME}:5672/%2F'
 
 QUEUE_NAME = 'links'
+redis_cache = redis.Redis(host="cache", port=6379, db=0)
+
+
+def get_link_status(link: str) -> str:
+    status = get_status_from_cache(link)
+    if status is None:
+        status = fetch_status_from_inet(link)
+        update_status_in_cache(link, status)
+    return status
+
+
+def get_status_from_cache(link: str) -> Optional[str]:
+    key = f"url-{link}"
+    value = redis_cache.get(key)
+    return None if value is None else value.decode("utf-8")
+
+
+def update_status_in_cache(link: str, status_code: str) -> None:
+    key = f"url-{link}"
+    redis_cache.set(name=key, value=status_code)
+
+
+def fetch_status_from_inet(link: str) -> str:
+    response = requests.get(link, timeout=10)
+    status = str(response.status_code)
+    return status
 
 
 def handle_message(ch, method, properties, body):
     try:
         body_str = body.decode('utf-8')
         link_json = json.loads(body_str)
-        response = requests.get(link_json['url'], timeout=10)
-        status = response.status_code
+        status = get_link_status(link_json['url'])
         payload = {'id': int(link_json['id']), 'status': str(status)}
         payload_json = json.dumps(payload)
         result = requests.put(f'http://{WEB_HOSTNAME}:{WEB_PORT}/links/', data=payload_json)
